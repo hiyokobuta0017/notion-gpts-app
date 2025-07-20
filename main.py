0f31d66d3c794af0a46f8b0e0c13a6af20e23e09
hiyokobuta0017 <137699164+hiyokobuta0017@users.noreply.github.com>
2025-07-20 22:03:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,17 @@
 from fastapi import FastAPI
 from notion_client import Client
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 app = FastAPI()
 
-# Notion クライアントの初期化
-notion = Client(auth=os.getenv("NOTION_TOKEN"))
-database_id = os.getenv("NOTION_DATABASE_ID")
+notion = Client(auth=os.environ["NOTION_TOKEN"])
 
 @app.get("/")
-def read_root():
+def root():
     return {"message": "Hello, Notion-GPTs!"}
 
 @app.get("/notion-database")
 def get_database():
-    try:
-        response = notion.databases.query(database_id=database_id)
-        return response
-    except Exception as e:
-        return {"error": str(e)}
+    database_id = os.environ["DATABASE_ID"]
+    response = notion.databases.query(database_id=database_id)
+    return response

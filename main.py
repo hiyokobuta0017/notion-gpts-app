1c1fa657123d61662d3238cc85863247dba6ca23
hiyokobuta0017 <137699164+hiyokobuta0017@users.noreply.github.com>
2025-07-21 22:50:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,24 +1,25 @@
-from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from notion_client import Client
 import os
+from fastapi import FastAPI
+from notion_client import Client
 from dotenv import load_dotenv
 
 load_dotenv()
 
-NOTION_TOKEN = os.getenv("NOTION_TOKEN")
-notion = Client(auth=NOTION_TOKEN)
-
 app = FastAPI()
+
+NOTION_TOKEN = os.getenv("NOTION_TOKEN")
+DATABASE_ID = os.getenv("DATABASE_ID")
+
+notion = Client(auth=NOTION_TOKEN)
 
 @app.get("/")
 def read_root():
     return {"message": "Hello, Notion-GPTs!"}
 
-@app.get("/notion-database/{database_id}")
-def get_database(database_id: str):
+@app.get("/notion-database")
+def get_database():
     try:
-        response = notion.databases.query(database_id=database_id)
+        response = notion.databases.query(database_id=DATABASE_ID)
         return response
     except Exception as e:
-        return JSONResponse(status_code=400, content={"error": str(e)})
+        return {"error": str(e)}
